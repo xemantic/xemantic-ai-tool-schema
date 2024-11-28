a3d28ba8ae5e69c05370701111c2fb5c0fb3b649
Kazik Pogoda <morisil@xemantic.com>
2024-11-28 20:13:17 +0100

    add TestSupport.kt

--- src/commonTest/kotlin/test/TestSupport.kt
+++ src/commonTest/kotlin/test/TestSupport.kt
@@ -1,11 +1,12 @@
-package com.xemantic.json.schema.test
+package com.xemantic.ai.tool.schema.test
 
-import com.xemantic.json.schema.serialization.jsonSchemaSerializersModule
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+/**
+ * A pretty printing [Json] for tests.
+ */
 val testJson = Json {
-  serializersModule = jsonSchemaSerializersModule
   prettyPrint = true
   @OptIn(ExperimentalSerializationApi::class)
   prettyPrintIndent = "  "
