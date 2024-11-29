765510f52dd493fbbb9e5946c2721d4e84886e9e
Kazik Pogoda <morisil@xemantic.com>
2024-11-29 15:11:21 +0100

    annotations documentation update

--- src/commonMain/kotlin/meta/JsonSchemaAnnotations.kt
+++ src/commonMain/kotlin/meta/JsonSchemaAnnotations.kt
@@ -6,8 +6,7 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.MetaSerializable
 
 /**
- * Adds
- * [title](https://json-schema.org/understanding-json-schema/reference/annotations) keyword
+ * Adds [title](https://json-schema.org/understanding-json-schema/reference/annotations) keyword
  * to generated JSON Schema of the property.
  *
  * A title will preferably be short, whereas a [Description] will provide a more lengthy explanation
@@ -16,8 +15,14 @@ import kotlinx.serialization.MetaSerializable
  * If the [Title] annotation is not found on the property, the JSON Schema generator will
  * attempt to read it from the class of the property.
  *
- * Note: quite often the property name is already self-explanatory, and does not require title, therefore, in practical
- * use cases, the [Description] might be used much more often than [Title].
+ * Note: Quite often the property name is already self-explanatory and does not require a title.
+ * In practical use cases, the [Description] might be used much more often than [Title].
+ *
+ * Example usage:
+ * ```
+ * @Title("User ID")
+ * val id: String
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS, AnnotationTarget.TYPE)
@@ -27,8 +32,7 @@ public annotation class Title(
 )
 
 /**
- * Adds
- * [description](https://json-schema.org/understanding-json-schema/reference/annotations) keyword
+ * Adds [description](https://json-schema.org/understanding-json-schema/reference/annotations) keyword
  * to generated JSON Schema of the property.
  *
  * The [Description] provides explanation about the purpose of the data described by the schema
@@ -37,8 +41,11 @@ public annotation class Title(
  * If the [Description] annotation is not found on the property, the JSON Schema generator will
  * attempt to read it from the class of the property.
  *
- * Note: quite often the property name is already self-explanatory, and does not require title, therefore, in practical
- * use cases, the [Description] might be used much more often than [Title].
+ * Example usage:
+ * ```
+ * @Description("A unique identifier for the user")
+ * val id: String
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS, AnnotationTarget.TYPE)
@@ -52,6 +59,14 @@ public annotation class Description(
  * Minimal length of the string.
  * Will add [minLength](https://json-schema.org/understanding-json-schema/reference/string#length)
  * keyword to generated JSON schema of the `string` property.
+ *
+ * Usually used in conjunction with [MaxLength].
+ *
+ * Example usage:
+ * ```
+ * @MinLength(8)
+ * val password: String
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -64,6 +79,14 @@ public annotation class MinLength(
  * Maximal length of the string.
  * Will add [maxLength](https://json-schema.org/understanding-json-schema/reference/string#length)
  * keyword to generated JSON schema of the `string` property.
+ *
+ * Usually used in conjunction with [MinLength].
+ *
+ * Example usage:
+ * ```
+ * @MaxLength(50)
+ * val username: String
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -76,6 +99,12 @@ public annotation class MaxLength(
  * The regex pattern the string must follow.
  * Will add [pattern](https://json-schema.org/understanding-json-schema/reference/string#regexp)
  * keyword to generated JSON schema of the `string` property.
+ *
+ * Example usage:
+ * ```
+ * @Pattern("[A-Z]{2}\\d{9}")
+ * val passportNumber: String
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -128,6 +157,12 @@ public annotation class Format(
  *
  * Note: this annotation should be used only for non-standard formats, which are not
  * covered by the [StringFormat] enum. For standard formats use the [Format] annotation.
+ *
+ * Example usage:
+ * ```
+ * @FormatString("color")
+ * val favoriteColor: String
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -172,6 +207,18 @@ public annotation class ContentMediaType(
  * then `exclusiveMinimum` keywords will be used instead.
  *
  * Usually it will be used together with [Max].
+ *
+ * @param value The minimum value allowed.
+ * @param exclusive Whether the minimum is exclusive (default is false).
+ *
+ * Example usage:
+ * ```
+ * @Min(0.0)
+ * val positiveNumber: Double
+ *
+ * @Min(1.0, exclusive = true)
+ * val greaterThanOne: Double
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -182,12 +229,24 @@ public annotation class Min(
 )
 
 /**
- * The minimal value of the number.
+ * The maximal value of the number.
  * Will add [maximum](https://json-schema.org/understanding-json-schema/reference/numeric#range)
  * keyword to generated JSON schema of the `number` property. If [exclusive] flag is `true`,
  * then `exclusiveMaximum` keywords will be used instead.
  *
  * Usually it will be used together with [Min].
+ *
+ * @param value The maximum value allowed.
+ * @param exclusive Whether the maximum is exclusive (default is false).
+ *
+ * Example usage:
+ * ```
+ * @Max(100.0)
+ * val percentage: Double
+ *
+ * @Max(1.0, exclusive = true)
+ * val lessThanOne: Double
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -217,6 +276,20 @@ public annotation class MultipleOf(
  * then `exclusiveMinimum` keywords will be used instead.
  *
  * Usually it will be used together with [MaxInt].
+ *
+ * Note: Use this for integer properties. For floating-point numbers, use [Min] instead.
+ *
+ * @param value The minimum value allowed.
+ * @param exclusive Whether the minimum is exclusive (default is false).
+ *
+ * Example usage:
+ * ```
+ * @MinInt(0)
+ * val nonNegativeInt: Int
+ *
+ * @MinInt(1, exclusive = true)
+ * val positiveInt: Int
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -259,6 +332,14 @@ public annotation class MultipleOfInt(
  * The minimum number of items property of the array.
  * Will add [minItems](https://json-schema.org/understanding-json-schema/reference/array#length)
  * keyword to generated JSON schema of the `array` property.
+ *
+ * Usually used in conjunction with [MaxItems].
+ *
+ * Example usage:
+ * ```
+ * @MinItems(1)
+ * val nonEmptyList: List<String>
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -271,6 +352,14 @@ public annotation class MinItems(
  * The maximum number of items property of the array.
  * Will add [maxItems](https://json-schema.org/understanding-json-schema/reference/array#length)
  * keyword to generated JSON schema of the `array` property.
+ *
+ * Usually used in conjunction with [MinItems].
+ *
+ * Example usage:
+ * ```
+ * @MaxItems(10)
+ * val limitedList: List<String>
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
@@ -283,6 +372,15 @@ public annotation class MaxItems(
  * The uniqueness of array items.
  * Will add [uniqueItems](https://json-schema.org/understanding-json-schema/reference/array#uniqueItems)
  * keyword to generated JSON schema of the `array` property.
+ *
+ * When set to true, all items in the array must be unique.
+ * Uniqueness is determined by comparing the JSON representation of each item.
+ *
+ * Example usage:
+ * ```
+ * @UniqueItems(true)
+ * val tags: List<String>
+ * ```
  */
 @OptIn(ExperimentalSerializationApi::class)
 @Target(AnnotationTarget.PROPERTY)
