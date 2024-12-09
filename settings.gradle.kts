0f6a510696a3527dcf8ada34cc56469006da4239
Kazik Pogoda <morisil@xemantic.com>
2024-12-09 16:20:47 +0100

    release process and tagging updated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
-rootProject.name = "xemantic-ai-tool-schema"
+val groupId = "com.xemantic.ai"
+val name = "xemantic-ai-tool-schema"
+val versionId = "0.1-SNAPSHOT"
+
+rootProject.name = name
+gradle.beforeProject {
+  group = groupId
+  version = versionId
+}
