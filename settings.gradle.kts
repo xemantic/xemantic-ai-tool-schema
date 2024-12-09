2d2d14a221fe8ff6e66715976d7ca9b6a43aab94
Kazik Pogoda <morisil@xemantic.com>
2024-12-09 17:00:19 +0100

    fix release process

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
 val groupId = "com.xemantic.ai"
 val name = "xemantic-ai-tool-schema"
-val versionId = "0.1-SNAPSHOT"
 
 rootProject.name = name
 gradle.beforeProject {
   group = groupId
-  version = versionId
 }
