209601fdeb6f20f443b5fa51276b4f48918c35f5
Kazik Pogoda <morisil@xemantic.com>
2025-02-01 23:39:33 +0100

    adjustments to jreleaser releases (#12)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,1 @@
-val groupId = "com.xemantic.ai"
-val name = "xemantic-ai-tool-schema"
-
-rootProject.name = name
-gradle.beforeProject {
-    group = groupId
-}
+rootProject.name = "xemantic-ai-tool-schema"
